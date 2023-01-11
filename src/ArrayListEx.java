f3634888fb1378cc64ae35f4b5a49c4318ed22b6
cosycolor <handyoon87@gmail.com>
2023-01-11 23:54:32 +0900

    ArrayList Second

--- src/ArrayListEx.java
+++ src/ArrayListEx.java
@@ -2,45 +2,60 @@ import java.util.*;
 
 public class ArrayListEx {
     public static void main(String[] args){
-        ArrayList list1 = new ArrayList();
-        list1.add(new Integer(5));
-        list1.add(new Integer(4));
-        list1.add(new Integer(2));
-        list1.add(new Integer(0));
-        list1.add(new Integer(1));
-        list1.add(new Integer(3));
+//        ArrayList list1 = new ArrayList();
+//        list1.add(new Integer(5));
+//        list1.add(new Integer(4));
+//        list1.add(new Integer(2));
+//        list1.add(new Integer(0));
+//        list1.add(new Integer(1));
+//        list1.add(new Integer(3));
+//
+//        ArrayList list2 = new ArrayList(list1.subList(1,4));
+//        print(list1, list2);
+//        Collections.sort(list1);
+//        Collections.sort(list2);
+//
+//        System.out.println("list1.containsAll(list2) :" + list1.containsAll(list2)  );
+//
+//        list2.add("B");
+//        list2.add("C");
+//        list2.add(3,"A");
+//        print(list1, list2);
+//
+//        list2.set(3,"AA");
+//        print(list1,list2);
+//
+//        System.out.println("list1.retainAll(list2) :"+ list1.retainAll(list2));
+//
+//        print(list1,list2);
+//
+//        for(int i = list2.size() -1; i >= 0; i--){
+//            if(list1.contains(list2.get(i))){
+//                list2.remove(i);
+//            }
+//            print(list1,list2);
+//        }
+        final int LIMIT = 10;
+        String source = "0123456789abcdefghijABCDEFGHIJ!@#$%^&*()ZZZ";
+        int length = source.length();
 
-        ArrayList list2 = new ArrayList(list1.subList(1,4));
-        print(list1, list2);
-        Collections.sort(list1);
-        Collections.sort(list2);
+        List list = new ArrayList(length/LIMIT+10);
 
-        System.out.println("list1.containsAll(list2) :" + list1.containsAll(list2)  );
-
-        list2.add("B");
-        list2.add("C");
-        list2.add(3,"A");
-        print(list1, list2);
-
-        list2.set(3,"AA");
-        print(list1,list2);
-
-        System.out.println("list1.retainAll(list2) :"+ list1.retainAll(list2));
-
-        print(list1,list2);
-
-        for(int i = list2.size() -1; i >= 0; i--){
-            if(list1.contains(list2.get(i))){
-                list2.remove(i);
+        for(int i =0; i< length; i+=LIMIT){
+            if(i+LIMIT < length){
+                list.add(source.substring(i,i+LIMIT));
+            }else{
+                list.add(source.substring(i));
             }
-            print(list1,list2);
         }
-
+        for(int i = 0; i < list.size();i++){
+            System.out.println(list.get(i));
+        }
     }
 
     static void print(ArrayList list1, ArrayList list2){
         System.out.println("list1:"+list1);
-        System.out.println("list1:"+list1);
+        System.out.println("list2:"+list2);
         System.out.println();
 
     }
